311525f463076b838c7c890e32c07b1259e2c13b
Christopher Hoch <chris.hoch@hpc.at>
2025-02-18 20:56:19 +0100

    switched to EEPROM based config instead of configuration.h

---
+++ fancoil_ctrl/src/configuration.h
@@ -0,0 +1,9 @@
+#ifndef CONFIGURATION
+#define CONFIGURATION
+
+#define AMBIENT_TEMPERATURE_TIMEOUT_S 1200 // if defined, the device will turn off after not receiving an ambient temperature after n seconds
+#define WIFI_MGR_EEPROM_START_ADDR 512
+#define LOAD_WATER_TEMP
+//#define USE_LOGGING
+
+#endif
